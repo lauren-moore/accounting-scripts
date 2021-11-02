4d81d73b0a51eaf5c86deb1bc5311404c4f8729d
Lauren <laurencaroleen@gmail.com>
2021-11-02 03:43:48 +0000

    Completed function with new attributes

--- melon_info.py
+++ melon_info.py
@@ -1,21 +1,16 @@
 """Print out all the melons in our inventory."""
 
+from melons import melons
 
-from melons import melon_names
 
-
-def print_melon(melon_names):
+def print_melon(melons):
     """Print each melon with corresponding attribute information."""
 
-    for key, value in melon_names.items():
-        print(f"{key}")
-
-    # have_or_have_not = 'have'
-    # if seedless:
-    #     have_or_have_not = 'do not have'
-
-    # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+    for key, value in melons.items():
+            print(f"{key.upper()}")
+            for attribute, value in value.items():
+                print(f"    {attribute}:{value}")
+            print()    
 
 
-for i in melon_names:
-    print_melon(melon_names[i])
+print_melon(melons)
--- melons.py
+++ melons.py
@@ -1,23 +1,41 @@
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
+melons = {
+    'Honeydew': {
+        "price": 0.99, 
+        "seedlessness": True,
+        "flesh_color": None,
+        "weight": None,
+        "rind_color": None,
+    },
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
+    'Crenshaw': {
+        "price": 2.00, 
+         "seedlessness": False,
+        "flesh_color": None,
+        "weight": None,
+        "rind_color": None,
+    },
+   
+     'Crane': {
+        "price": 2.50, 
+        "seedlessness": False,
+        "flesh_color": None,
+        "weight": None,
+        "rind_color": None,
+    },
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
+     'Casaba': {
+        "price": 2.50, 
+        "seedlessness": False,
+        "flesh_color": None,
+        "weight": None,
+        "rind_color": None,
+    },
+   
+    'Cantaloupe': {
+        "price": 0.99, 
+        "seedlessness": False,
+        "flesh_color": None,
+        "weight": None,
+        "rind_color": None,
+    } 
 }
